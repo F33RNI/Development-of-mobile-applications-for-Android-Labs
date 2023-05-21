bad0373c2a1b6245de0f3ed0bad416ce43bb0056
F33RNI <xxoinvizionxx@gmail.com>
2023-05-22 01:38:45 +0300

    Fix LR 6

--- LR6_supernew/app/src/main/java/com/example/lr6_supernew/MainActivity.java
+++ LR6_supernew/app/src/main/java/com/example/lr6_supernew/MainActivity.java
@@ -1,10 +1,5 @@
 package com.example.lr6_supernew;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
@@ -17,155 +12,87 @@ import android.graphics.drawable.shapes.RoundRectShape;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
-
-             return true;
+        return true;
     }
-    //public boolean onOptionsItemSelected(MenuItem item) {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        final ConstraintLayout mylayout =
-                (ConstraintLayout)findViewById(R.id.root);
+        ShapeDrawable shapeDrawable;
 
-        ShapeDrawable d;
         switch (item.getItemId()) {
             case R.id.rectangle:
-                d = new ShapeDrawable(new RectShape());
-                d.setIntrinsicHeight(2);
-                d.setIntrinsicWidth(150);
-                d.getPaint().setColor(Color.MAGENTA);
+                shapeDrawable = new ShapeDrawable(new RectShape());
+                shapeDrawable.setIntrinsicHeight(2);
+                shapeDrawable.setIntrinsicWidth(15);
+                shapeDrawable.getPaint().setColor(Color.MAGENTA);
 
+                imageView.setImageDrawable(shapeDrawable);
                 return true;
+
             case R.id.star:
-                Path p = new Path();
-                p.moveTo(50, 0);
-                p.lineTo(25,100);
-                p.lineTo(100,50);
-                p.lineTo(0,50);
-                p.lineTo(75,100);
-                p.lineTo(50,0);
-                d = new ShapeDrawable(new PathShape(p, 100, 100));
-                d.setIntrinsicHeight(200);
-                d.setIntrinsicWidth(200);
-                d.getPaint().setColor(Color.RED);
-                d.getPaint().setStyle(Paint.Style.STROKE);
+                Path path = new Path();
+                path.moveTo(50, 0);
+                path.lineTo(25, 100);
+                path.lineTo(100, 50);
+                path.lineTo(0, 50);
+                path.lineTo(75, 100);
+                path.lineTo(50, 0);
 
+                shapeDrawable = new ShapeDrawable(new PathShape(path, 100, 100));
+                shapeDrawable.setIntrinsicHeight(200);
+                shapeDrawable.setIntrinsicWidth(200);
+                shapeDrawable.getPaint().setColor(Color.RED);
+                shapeDrawable.getPaint().setStyle(Paint.Style.STROKE);
+
+                imageView.setImageDrawable(shapeDrawable);
                 return true;
+
             case R.id.oval:
-                float[] outR = new float[] { 10, 10, 50, 50, 20, 20, 20, 20 };
+                float[] outR = new float[]{10, 10, 50, 50, 20, 20, 20, 20};
                 RectF rectF = new RectF(10, 10, 10, 10);
-                float[] inR = new float[] { 40, 40, 40, 40, 40, 40, 40, 40 };
-                d = new ShapeDrawable(new RoundRectShape(outR, rectF ,
-                        inR));
-                d.setIntrinsicHeight(300);
-                d.setIntrinsicWidth(500);
-                d.getPaint().setColor(Color.BLUE);
+                float[] inR = new float[]{40, 40, 40, 40, 40, 40, 40, 40};
+                shapeDrawable = new ShapeDrawable(new RoundRectShape(outR, rectF, inR));
+                shapeDrawable.setIntrinsicHeight(300);
+                shapeDrawable.setIntrinsicWidth(500);
+                shapeDrawable.getPaint().setColor(Color.BLUE);
 
-
+                imageView.setImageDrawable(shapeDrawable);
                 return true;
 
             case R.id.duga:
-                d = new ShapeDrawable(new ArcShape(0, 255));
-                d.setIntrinsicHeight(200);
-                d.setIntrinsicWidth(200);
-                d.getPaint().setColor(Color.RED);
+                shapeDrawable = new ShapeDrawable(new ArcShape(0, 255));
+                shapeDrawable.setIntrinsicHeight(200);
+                shapeDrawable.setIntrinsicWidth(200);
+                shapeDrawable.getPaint().setColor(Color.RED);
+
+                imageView.setImageDrawable(shapeDrawable);
+                return true;
 
             case R.id.exit:
                 finish();
                 return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
-
-
     }
-    private void drawShape(ShapeDrawable shapeDrawable) {
-        shapeDrawable = new ShapeDrawable(new RectShape());
-        shapeDrawable.setIntrinsicHeight(2);
-        shapeDrawable.setIntrinsicWidth(150);
-        shapeDrawable.getPaint().setColor(Color.MAGENTA);
-
-        View shapeView = new View(this) {
-            @Override
-            protected void onDraw(Canvas canvas) {
-                super.onDraw(canvas);
-                shapeDrawable.setBounds(new Rect(0, 0, getWidth(), getHeight()));
-                shapeDrawable.draw(canvas);
-            }
-        };
-
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-        );
-
-        containerLayout.addView(shapeView, layoutParams);
-    }
-
-    /*public boolean onMenuItemClick(@NonNull MenuItem item) {
-        final ConstraintLayout mylayout =
-                (ConstraintLayout)findViewById(R.id.root);
-        ShapeDrawable d = new ShapeDrawable();
-        switch (item.getItemId()) {
-            case R.id.rectangle:
-                d = new ShapeDrawable(new RectShape());
-                d.setIntrinsicHeight(2);
-                d.setIntrinsicWidth(150);
-                d.getPaint().setColor(Color.MAGENTA);
-                return true;
-            case R.id.star:
-                Path p = new Path();
-                p.moveTo(50, 0);
-                p.lineTo(25,100);
-                p.lineTo(100,50);
-                p.lineTo(0,50);
-                p.lineTo(75,100);
-                p.lineTo(50,0);
-                d = new ShapeDrawable(new PathShape(p, 100, 100));
-                d.setIntrinsicHeight(200);
-                d.setIntrinsicWidth(200);
-                d.getPaint().setColor(Color.RED);
-                d.getPaint().setStyle(Paint.Style.STROKE);
-
-                return true;
-            case R.id.oval:
-                float[] outR = new float[] { 10, 10, 50, 50, 20, 20, 20, 20 };
-                RectF rectF = new RectF(10, 10, 10, 10);
-                float[] inR = new float[] { 40, 40, 40, 40, 40, 40, 40, 40 };
-                d = new ShapeDrawable(new RoundRectShape(outR, rectF ,
-                        inR));
-                d.setIntrinsicHeight(300);
-                d.setIntrinsicWidth(500);
-                d.getPaint().setColor(Color.BLUE);
-
-
-                return true;
-
-            case R.id.duga:
-                d = new ShapeDrawable(new ArcShape(0, 255));
-                d.setIntrinsicHeight(200);
-                d.setIntrinsicWidth(200);
-                d.getPaint().setColor(Color.RED);
-
-            case R.id.exit:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }*/
 }
