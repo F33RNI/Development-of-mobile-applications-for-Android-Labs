2e6cf8bf3f89fddc0530918aad3c1ee6f2a8d402
Polina <poly.nika22@gmail.com>
2023-05-23 01:58:34 +0300

    i hate fucking androids(tudio). Fucking_cube is final version

--- LR7/1st_try/app/src/main/java/com/example/cube/MyGLActivity.java
+++ LR7/1st_try/app/src/main/java/com/example/cube/MyGLActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class MyGLActivity extends Activity {
+    //
 
     private GLSurfaceView glView;                   // use GLSurfaceView
 
---
+++ LR7/Fucking_cube/app/src/main/java/com/example/fucking_cube/OpenGLDemoActivity.java
@@ -0,0 +1,24 @@
+package com.example.fucking_cube;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class OpenGLDemoActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Go fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new OpenGLRenderer());
+        setContentView(view);
+    }
+}
