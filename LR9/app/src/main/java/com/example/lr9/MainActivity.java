ce43aad016b7021b4b4d3a2b61eb4c3c8e45f757
F33RNI <xxoinvizionxx@gmail.com>
2023-06-15 22:01:40 +0300

    LR9 working update

--- LR9/app/src/main/java/com/example/lr9/MainActivity.java
+++ LR9/app/src/main/java/com/example/lr9/MainActivity.java
@@ -1,3 +1,30 @@
+/**
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
 package com.example.lr9;
 
 import android.content.Context;
@@ -18,8 +45,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private TextView txt_XYZ, txt_Lt;
+    // System service for managing sensors
     private SensorManager sensorManager;
+
+    // Search all sensors with ID up to this value
+    private static final int SENSOR_ID_MAX = 1000;
 
     private final List<Sensor> sensorsList = new ArrayList<>();
     private final List<TextView> textViewsList = new ArrayList<>();
@@ -32,50 +62,55 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Get system service
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        LinearLayout linearLayout = findViewById(R.id.sensorsData);
-
+        // Check if we have this service
         if (sensorManager != null) {
-            for (int i = 0; i < 1000; i++) {
+            // Get linear layout from activity
+            LinearLayout linearLayout = findViewById(R.id.sensorsData);
+
+            // Iterate all possible sensors
+            for (int i = 0; i < SENSOR_ID_MAX; i++) {
+                // Try to get them
                 try {
                     Sensor sensor = sensorManager.getDefaultSensor(i);
+
+                    // Check if sensor has name
                     if (sensor.getName().length() > 1) {
+                        // Try to add it to the List
                         sensorsList.add(sensor);
 
+                        // Create new TextView for sensor data
                         TextView textView = new TextView(this);
 
+                        // Set sensor name
+                        textView.setText(String.format("Sensor name: %s\nNo data yet...\n",
+                                sensor.getName()));
+
+                        // Add this TextView to the LinearLayout
                         linearLayout.addView(textView);
 
+                        // Also, add it to the List of text views
                         textViewsList.add(textView);
                     }
                 } catch (Exception ignored) { }
             }
 
+            // Connect listeners
             registerListeners();
+        }
 
-
-            //Sensor sensor =
-            /*mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-            if (mAccelerometer != null) {
-                mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-            } else {
-                Toast.makeText(this, "No accelerometer sensor detected", Toast.LENGTH_SHORT).show();
-            }
-
-            Sensor lightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-
-            if (lightSensor != null) {
-                mSensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
-            } else {
-                Toast.makeText(this, "No light sensor detected", Toast.LENGTH_SHORT).show();
-            }*/
-        } else {
-            Toast.makeText(this, "Sensor service not available", Toast.LENGTH_SHORT).show();
+        // We don't have service for sensors =(
+        else {
+            Toast.makeText(this, "Sensor service not available",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
+    /**
+     * Registers (connects) listener for all sensors from List
+     */
     private void registerListeners() {
         if (sensorManager != null) {
             for (Sensor sensor: sensorsList) {
@@ -86,49 +121,56 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
+    /**
+     * Retrieves TextView from textViewsList based on Sensor object from sensorsList
+     * @param sensor Sensor object (from sensorsList)
+     * @return TextView (textViewsList)
+     */
     private TextView getTextViewBySensorID(Sensor sensor) {
+        // Try to find sensor
         for (int i = 0; i < sensorsList.size(); i++) {
             if (sensorsList.get(i).equals(sensor)) {
                 return textViewsList.get(i);
             }
-
         }
+
+        // No sensor found
         return null;
     }
 
+    /**
+     * Updates sensor's data into TextViews
+     * @param sensorEvent SensorEvent object (from connected listener)
+     */
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        // Try to get matching TextView
         TextView textView = getTextViewBySensorID(sensorEvent.sensor);
-        if (textView != null) {
-            textView.setText("Sensor: " + sensorEvent.sensor.getName() + " values: " + Arrays.toString(sensorEvent.values));
-        }
 
-
-        //System.out.println();
-
-
-        /*if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            txt_XYZ.setText("X: " + sensorEvent.values[0] + ", Y: " + sensorEvent.values[1]
-                    + ", Z: " + sensorEvent.values[2]);
-        } else if (sensorEvent.sensor.getType() == Sensor.TYPE_LIGHT) {
-            txt_Lt.setText("Value: " + sensorEvent.values[0]);
-        }*/
+        // Check if TextView exists
+        if (textView != null)
+            textView.setText(String.format("Sensor name: %s\nSensor data: %s\n",
+                    sensorEvent.sensor.getName(), Arrays.toString(sensorEvent.values)));
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Не нужно делать ничего в этом методе
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        // Register listeners back after activity resuming
         registerListeners();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
+        // Unregister listeners on activity pause
         sensorManager.unregisterListener(this);
     }
 }
