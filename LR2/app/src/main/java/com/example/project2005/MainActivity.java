adb003a8f8b90bf5444653e9b93e5a6b5549a88b
F33RNI <xxoinvizionxx@gmail.com>
2023-06-19 13:18:40 +0300

    LR2 Fix

--- LR2/app/src/main/java/com/example/project2005/MainActivity.java
+++ LR2/app/src/main/java/com/example/project2005/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.project2005;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout constraintLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView my_text = (TextView) findViewById(R.id.my_text);
-        my_text.setTypeface(Typeface.createFromAsset(
-                getAssets(), "fonts/penguinattackcyrillic.otf"));
+
+        // Define root layout
+        constraintLayout = findViewById(R.id.root);
+
+        // Create a typeface from file
+        Typeface font = Typeface.createFromAsset(getAssets(),
+                "fonts/DotGothic16-Regular.ttf");
+
+        // Set typeface
+        ((TextView) findViewById(R.id.textView)).setTypeface(font);
     }
 
     @Override
@@ -25,26 +34,30 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Menu callback
+     * @param item MenuItem
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        final ConstraintLayout mylayout =
-                (ConstraintLayout) findViewById(R.id.root);
         switch (item.getItemId()) {
             case R.id.red:
-
-                mylayout.setBackgroundColor(getResources().getColor(R.color.red));
+                constraintLayout.setBackgroundColor(getResources().getColor(R.color.red));
                 return true;
+
             case R.id.green:
-
-                mylayout.setBackgroundColor(getResources().getColor(R.color.green));
+                constraintLayout.setBackgroundColor(getResources().getColor(R.color.green));
                 return true;
+
             case R.id.blue:
-
-                mylayout.setBackgroundColor(getResources().getColor(R.color.blue));
+                constraintLayout.setBackgroundColor(getResources().getColor(R.color.blue));
                 return true;
+
             case R.id.exit:
                 finish();
                 return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
