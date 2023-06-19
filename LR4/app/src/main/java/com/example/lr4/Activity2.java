2bb83c9f3f50ea26a6e34b0f39216bbfd578dad7
F33RNI <xxoinvizionxx@gmail.com>
2023-06-19 17:36:08 +0300

    LR4 Fix

--- LR4/app/src/main/java/com/example/lr4/Activity2.java
+++ LR4/app/src/main/java/com/example/lr4/Activity2.java
@@ -1,9 +1,5 @@
 package com.example.lr4;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -12,110 +8,119 @@ import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class Activity2 extends AppCompatActivity {
-    private Button button2;
+    private static final int REQUEST_CODE = 1;
+
     private TextView phonebook;
-    private ArrayList<String> arrayList;
-    private TextView callbackStatusTextView; // TextView для отображения информации о методах обратного вызова
+    private TextView callbackStatusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
+        // Contacts
         phonebook = findViewById(R.id.phonebook);
-        arrayList = new ArrayList<>();
-        callbackStatusTextView = findViewById(R.id.callback_status); // Инициализация TextView для отображения информации о методах обратного вызова
 
+        // Logging TextView
+        callbackStatusTextView = findViewById(R.id.callback_status);
+
+        // Request contacts permissions
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
                 && checkSelfPermission(Manifest.permission.READ_CONTACTS)
                 != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 1);
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_CODE);
         } else {
             getContact();
         }
 
-        button2 = findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity1();
-            }
-        });
+        // Open call screen activity (MainActivity)
+        findViewById(R.id.button2).setOnClickListener(v ->
+                startActivity(new Intent(Activity2.this, MainActivity.class)));
 
-        updateCallbackStatus("onCreate"); // Обновление информации о методе обратного вызова onCreate
-    }
-
-    private void openActivity1() {
-        Intent intent = new Intent(Activity2.this, MainActivity.class);
-        startActivity(intent);
+        // Log method call
+        callbackStatusTextView.append("onCreate()\n");
     }
 
     private void getContact() {
         Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                 null, null, null, null);
 
+        ArrayList<String> contactsList = new ArrayList<>();
+
         if (cursor != null && cursor.moveToFirst()) {
             do {
-                @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME_PRIMARY));
-                @SuppressLint("Range") String mobile = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                arrayList.add(name + " - " + mobile);
+                // Get contact name
+                @SuppressLint("Range") String name = cursor.getString(
+                        cursor.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME_PRIMARY));
+
+                // Get contact phone
+                @SuppressLint("Range") String mobile = cursor.getString(
+                        cursor.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                // Add to list
+                contactsList.add(name + " - " + mobile);
             } while (cursor.moveToNext());
 
             cursor.close();
         }
 
-        phonebook.setText(arrayList.toString());
+        // Convert list to string and add contacts to the TextView
+        phonebook.setText(contactsList.toString());
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+        // Log method call
+        callbackStatusTextView.append("onRequestPermissionsResult()\n");
+
+        // Get list of contacts
+        if (requestCode == REQUEST_CODE && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             getContact();
         }
-        updateCallbackStatus("onRequestPermissionsResult"); // Обновление информации о методе обратного вызова onRequestPermissionsResult
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        updateCallbackStatus("onStart"); // Обновление информации о методе обратного вызова onStart
+        callbackStatusTextView.append("onStart()\n");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        updateCallbackStatus("onResume"); // Обновление информации о методе обратного вызова onResume
+        callbackStatusTextView.append("onResume()\n");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        updateCallbackStatus("onPause"); // Обновление информации о методе обратного вызова onPause
+        callbackStatusTextView.append("onPause()\n");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        updateCallbackStatus("onStop"); // Обновление информации о методе обратного вызова onStop
+        callbackStatusTextView.append("onStop()\n");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        updateCallbackStatus("onDestroy"); // Обновление информации о методе обратного вызова onDestroy
-    }
-
-    private void updateCallbackStatus(String callbackMethod) {
-        String currentStatus = callbackStatusTextView.getText().toString();
-        String updatedStatus = currentStatus + "\n" + callbackMethod;
-        callbackStatusTextView.setText(updatedStatus);
+        callbackStatusTextView.append("onDestroy()\n");
     }
 }
--- LR4/app/src/main/java/com/example/lr4/MainActivity.java
+++ LR4/app/src/main/java/com/example/lr4/MainActivity.java
@@ -1,102 +1,82 @@
 package com.example.lr4;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-    private EditText phoneNo;
-    private FloatingActionButton callbtn;
-    private static final int PERMISSION_CODE = 100;
-    private TextView callbackStatusTextView; // TextView для отображения информации о методах обратного вызова
+    private static final int REQUEST_CODE = 1;
+
+    private EditText phoneNumber;
+    private TextView callbackStatusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        phoneNo = findViewById(R.id.editTextPhone);
-        callbtn = findViewById(R.id.floatingActionButton);
-        callbackStatusTextView = findViewById(R.id.callback_status2); // Инициализация TextView для отображения информации о методах обратного вызова
+        // Call number EditText
+        phoneNumber = findViewById(R.id.editTextPhone);
 
-        if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.CALL_PHONE}, PERMISSION_CODE);
+        // Logging TextView
+        callbackStatusTextView = findViewById(R.id.callback_status2);
+
+        // Grant call permissions
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{android.Manifest.permission.CALL_PHONE}, REQUEST_CODE);
         }
 
-        callbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String phoneno = phoneNo.getText().toString();
-                Intent intent2 = new Intent(Intent.ACTION_CALL);
-                intent2.setData(Uri.parse("tel:" + phoneno));
-                startActivity(intent2);
-            }
+        // Start call intent by pressing call button
+        findViewById(R.id.floatingActionButton).setOnClickListener(v -> {
+            Intent intent2 = new Intent(Intent.ACTION_CALL);
+            intent2.setData(Uri.parse("tel:" + phoneNumber.getText().toString()));
+            startActivity(intent2);
         });
 
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivity2();
-            }
-        });
+        // Open phone book activity
+        findViewById(R.id.button).setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), Activity2.class)));
 
-        updateCallbackStatus2("onCreate"); // Обновление информации о методе обратного вызова onCreate
-    }
-
-    public void openActivity2() {
-        Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
+        // Log method call
+        callbackStatusTextView.append("onCreate()\n");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        updateCallbackStatus2("onStart"); // Обновление информации о методе обратного вызова onStart
+        callbackStatusTextView.append("onStart()\n");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        updateCallbackStatus2("onResume"); // Обновление информации о методе обратного вызова onResume
+        callbackStatusTextView.append("onResume()\n");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        updateCallbackStatus2("onPause"); // Обновление информации о методе обратного вызова onPause
+        callbackStatusTextView.append("onPause()\n");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        updateCallbackStatus2("onStop"); // Обновление информации о методе обратного вызова onStop
+        callbackStatusTextView.append("onStop()\n");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        updateCallbackStatus2("onDestroy"); // Обновление информации о методе обратного вызова onDestroy
-    }
-
-    private void updateCallbackStatus2(String callbackMethod) {
-        String currentStatus = callbackStatusTextView.getText().toString();
-        String updatedStatus = currentStatus + "\n" + callbackMethod;
-        callbackStatusTextView.setText(updatedStatus);
+        callbackStatusTextView.append("onDestroy()\n");
     }
 }
